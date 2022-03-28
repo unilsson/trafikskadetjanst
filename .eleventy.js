891933f1e20bc8c3720a8381bde7fc3eee69be9f
unilsson <ulf.s.nilsson@gmail.com>
2022-03-29 00:27:25 +0200

    Adding new images for Services page

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
   // Copy the `img/` directory 
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("javascript");
+    eleventyConfig.addPassthroughCopy("circulator-images");
+    
     
     eleventyConfig.addPassthroughCopy("css");
 };
